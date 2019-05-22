de683639a872b12eb201ea8639b359a649bf92a5
Sheila Leveille <sbleveille@gmail.com>
2019-05-22 13:37:23 +1000

    Mock up for Yelp Exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning DB'
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name: 'Chipotle',
+    address: 'Atlanta, GA',
+    phone_number: '12345',
+    category: 'belgian'
+  },
+  {
+    name: 'Chick-fil-A',
+    address: 'San Francisco, CA',
+    phone_number: '12345678',
+    category: 'french'
+  },
+  {
+    name: 'Yum Yum',
+    address: 'Melbourne, VIC',
+    phone_number: '12345675',
+    category: 'chinese'
+  },
+  {
+    name: 'Hutong',
+    address: 'Melbourne, VIC',
+    phone_number: '09746782',
+    category: 'chinese'
+  },
+  {
+    name: 'Three Broomsticks',
+    address: 'Hogwarts',
+    phone_number: '6678933',
+    category: 'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Yum!'
